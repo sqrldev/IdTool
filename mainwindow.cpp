ca06b892411e6a75b1cffe618513ec4c5b4f0035
Alexander Hauser <alex@hausers.cc>
2019-09-02 12:09:05 +0200

    Add open file dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QStandardPaths>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->actionOpenFile, &QAction::triggered, this, &MainWindow::openFile);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::openFile()
+{
+    QString dir = nullptr;
+
+    const QStringList dirs = QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation);
+    if (dirs.count() > 0)
+    {
+        dir = QDir(dirs[0]).filePath("SQRL/");
+    }
+
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Image"), dir, tr("SQRL identity files (*.sqrl *.sqrc)"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void openFile();
+
 private:
     Ui::MainWindow *ui;
 };
