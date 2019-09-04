1d00064ce24bfe0a17a34ad433064283b700746c
Alexander Hauser <alex@hausers.cc>
2019-09-04 15:51:50 +0200

    WIP on IdentityModel

--- identitymodel.h
+++ identitymodel.h
@@ -2,15 +2,99 @@
 #define IDENTITYMODEL_H
 
 #include <QString>
+#include <QApplication>
+#include <QTranslator>
 #include <vector>
+
+enum EItemDataType
+{
+    UINT_8,
+    UINT_16,
+    UINT_32,
+    BYTE_ARRAY,
+    UNDEFINED
+};
 
 class IdentityBlockItem
 {
 public:
-    QString name;
-    QString description;
-    QString type;
-    int bytes;
+    QString name = "";
+    QString description = "";
+    EItemDataType type = EItemDataType::UNDEFINED;
+    int bytes = -1;
+    QString value = "";
+
+    bool parseValue(std::vector<char> data, int offset)
+    {
+        if (name == "" ||
+            description == "" ||
+            type == EItemDataType::UNDEFINED ||
+            bytes == -1)
+        {
+            return false;
+        }
+
+        if (data.size() < (offset + bytes))
+        {
+            return false;
+        }
+
+        switch (type)
+        {
+        case EItemDataType::UINT_8:
+            if (bytes != 1)
+            {
+                throw std::runtime_error("Invalid byte count for datatype UINT_8!");
+                return false;
+            }
+            value = parseUint8(data, offset);
+            return true;
+
+        case EItemDataType::UINT_16:
+            if (bytes != 2) throw std::runtime_error("Invalid byte count for datatype UINT_16!");
+            value = parseUint16(data, offset);
+            break;
+
+        case EItemDataType::UINT_32:
+            if (bytes != 4) throw std::runtime_error("Invalid byte count for datatype UINT_32!");
+            value = parseUint32(data, offset);
+            break;
+
+        case EItemDataType::BYTE_ARRAY:
+            if (bytes < 1) throw std::runtime_error("Invalid byte count for datatype BYTE_ARRAY!");
+            value = parseByteArray(data, offset, bytes);
+            break;
+        }
+    }
+
+    QString parseUint8(std::vector<char> data, int offset)
+    {
+        if ((data.size() - offset) < 1) return "";
+        return QString::number(data[offset]);
+    }
+
+    QString parseUint16(std::vector<char> data, int offset)
+    {
+        if ((data.size() - offset) < 2) return "";
+        return QString::number(data[offset] | (data[offset+1] << 8));
+    }
+
+    QString parseUint32(std::vector<char> data, int offset)
+    {
+        if ((data.size() - offset) < 4) return "";
+        return QString::number(data[offset] | (data[offset+1] << 8) | (data[offset+2] << 16) | (data[offset+3] << 24));
+    }
+
+    QString parseByteArray(std::vector<char> data, int offset, int bytes)
+    {
+        if ((data.size() - offset - bytes) < 1) return "";
+
+        for (int i=offset; i<(offset + bytes); i++)
+        {
+            //TODO: Base64-encode!!
+        }
+        return "";
+    }
 };
 
 class IdentityBlock
