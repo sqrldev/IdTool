aad27ad97e9d22b329462b77e2dfc23640e5b256
Alexander Hauser <alex@hausers.cc>
2019-09-06 22:31:39 +0200

    Remove unnecessary c-tors in IdentityModel

--- identitymodel.h
+++ identitymodel.h
@@ -24,16 +24,6 @@ public:
         int blockType = -1;
         std::string description = "";
         std::vector<IdentityBlockItem> items;
-
-        // C-tor
-        IdentityBlock() {}
-        // Copy-C-tor
-        IdentityBlock(const IdentityBlock &block2)
-        {
-            blockType = block2.blockType;
-            description = block2.description;
-            items = block2.items;
-        }
     };
 
     class IdentityBlockItem
@@ -57,19 +47,6 @@ public:
         std::string type = "UNDEFINED";
         int bytes = 0;
         std::string value = "";
-
-    public:
-        // C-tor
-        IdentityBlockItem() {}
-        // Copy-C-tor
-        IdentityBlockItem(const IdentityBlockItem &item2)
-        {
-            name = item2.name;
-            description = item2.description;
-            type = item2.type;
-            bytes = item2.bytes;
-            value = item2.value;
-        }
     };
 };
 
