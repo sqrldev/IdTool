a74e55a05623ec24392b94c5c27a349e6bf4541a
Alexander Hauser <alex@hausers.cc>
2019-09-13 21:36:34 +0200

    Replace deprecated call to QApplication::desktop()->screenGeometry()

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    QRect screenGeometry = QApplication::desktop()->screenGeometry();
+    QRect screenGeometry = QGuiApplication::screens().first()->geometry(); //QApplication::desktop()->screenGeometry();
     int x = (screenGeometry.width() - w.width()) / 2;
     w.move(x, w.y() + 20);
 
